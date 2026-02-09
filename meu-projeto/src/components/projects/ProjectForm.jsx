510d8ed3dba19c5a809d6833265c4d48f4b5a9cf
Erick Vessoli <143609058+er1ck-xyz@users.noreply.github.com>
2026-02-09 17:56:31 -0300

    Set up Form component

---
+++ meu-projeto/src/components/projects/ProjectForm.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./ProjectForm.module.css";
+import Input from "../form/input";
+import Select from "../form/Select";
+import SubmitButton from "../form/SubmitButton";
+
+export default function ProjectForm({ btnText }) {
+  return (
+    <form className={styles.form}>
+      <Input
+        type="text"
+        text="Nome do projeto"
+        name="name"
+        placeholder="Insira o nome do projeto"
+      />
+      <Input
+        type="number"
+        text="Orçamento do projeto"
+        name="budget"
+        placeholder="Insira o orçamento do projeto"
+      />
+      <div>
+        <Select name="category_id" text="Selecione a categoria" />
+      </div>
+      <SubmitButton text={btnText} />
+    </form>
+  );
+}
