1722bb59c597e766cfdce376a0f0464cdbaa7525
Erick Vessoli <143609058+er1ck-xyz@users.noreply.github.com>
2026-02-09 16:53:17 -0300

    Config Home and css

--- meu-projeto/src/pages/Home.jsx
+++ meu-projeto/src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import styles from "./Home.module.css";
+import savings from "../img/savings.svg";
+import LinkButton from "../components/layouts/LinkButton";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Pagina home</h1>
-    </div>
+    <section className={styles.home_container}>
+      <h1>
+        Bem-vindo ao <span>Costs</span>
+      </h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <LinkButton to="/newproject" text="Criar projeto" />
+      <img src={savings} alt="Costs" />
+    </section>
   );
 }
