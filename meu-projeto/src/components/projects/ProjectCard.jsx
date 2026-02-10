a09671406ee85881ca054e2a1c5860521f483e42
Erick Vessoli <143609058+er1ck-xyz@users.noreply.github.com>
2026-02-10 18:06:19 -0300

    Component card view

---
+++ meu-projeto/src/components/projects/ProjectCard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import style from "./ProjectCard.module.css";
+import { Link } from "react-router-dom";
+import { BsPencil, BsFillTrashFill } from "react-icons/bs";
+
+export default function ProjectCard({
+  id,
+  name,
+  budget,
+  category,
+  handleRemove,
+}) {
+  const remove = (e) => {
+    e.preventDefault();
+    handleRemove(id);
+    alert("Projeto excluido com sucesso!");
+  };
+  return (
+    <div className={style.project_card}>
+      <h4>{name}</h4>
+      <p>
+        <span>Orçamento: </span> R${budget}
+      </p>
+      <p className={style.category_text}>
+        <span className={`${style[category.toLowerCase()]}`}></span> {category}
+      </p>
+      <div className={style.project_card_actions}>
+        <Link to="/">
+          <BsPencil /> Editar
+        </Link>
+        <button onClick={remove}>
+          <BsFillTrashFill /> Excluir
+        </button>
+      </div>
+    </div>
+  );
+}
